19220bdcc199bdd1c47c86c78b7eda1bc277c62a
aymanebiti2 <aymanebiti2@gmail.com>
2022-09-18 13:19:10 -0700

    backend rest

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'reference'=>'required',
+            'description' => 'required',
+            'quantite'=>'required',
+            'prix'=>'required',
+        ]);
+        $product = Product::create($request->all());
+        return response()->json($product,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $product = Product::findOrFail($id);
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $request->validate([
+            'reference'=>'required',
+            'description' => 'required',
+            'quantite'=>'required',
+            'prix'=>'required',
+        ]);
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+        return response()->json($product,201);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return response('deleted with success');
+    }
+}
